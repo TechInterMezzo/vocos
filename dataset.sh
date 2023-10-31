a8db0e6590c8689dbddc38a5807a96cf105e4051
TechInterMezzo <ts.contact@t-online.de>
2023-10-31 09:09:39 +0100

    remove test dataset for now

--- dataset.sh
+++ dataset.sh
@@ -4,9 +4,9 @@ wget https://www.openslr.org/resources/60/train-clean-100.tar.gz
 tar -xf --no-same-permissions train-clean-100.tar.gz
 rm train-clean-100.tar.gz
 
-wget https://www.openslr.org/resources/60/test-clean.tar.gz
-tar -xf --no-same-permissions test-clean.tar.gz
-rm test-clean.tar.gz
+#wget https://www.openslr.org/resources/60/test-clean.tar.gz
+#tar -xf --no-same-permissions test-clean.tar.gz
+#rm test-clean.tar.gz
 
 find $(pwd)/LibriTTS/train-clean-100 -name *.wav > filelist.train
-find $(pwd)/LibriTTS/test-clean -name *.wav > filelist.val
+#find $(pwd)/LibriTTS/test-clean -name *.wav > filelist.val
