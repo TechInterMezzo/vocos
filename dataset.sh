25a3f597cf662573e4a2a12f7640c586673d36cc
TechInterMezzo <ts.contact@t-online.de>
2023-10-31 09:33:47 +0100

    fix dataset extraction

--- dataset.sh
+++ dataset.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 wget https://www.openslr.org/resources/60/train-clean-100.tar.gz
-tar --no-same-permissions -xf train-clean-100.tar.gz
+tar --no-same-owner --no-same-permissions -xf train-clean-100.tar.gz
 rm train-clean-100.tar.gz
 
 #wget https://www.openslr.org/resources/60/test-clean.tar.gz
-#tar  --no-same-permissions -xf test-clean.tar.gz
+#tar --no-same-owner --no-same-permissions -xf test-clean.tar.gz
 #rm test-clean.tar.gz
 
 find $(pwd)/LibriTTS/train-clean-100 -name *.wav > filelist.train
